3ff22e88b325921912484e1dad9f59aa9734d6bf
Harvey <harveyli43@163.com>
2026-04-24 14:15:07 +0800

    add 2.OpenAI库的基础使用.py  3.流式输出.py  4.OpenAI库附带历史消息调用模型.py

--- RAG与agent开发/3.流式输出.py
+++ RAG与agent开发/3.流式输出.py
@@ -3,7 +3,7 @@ import os
 
 # 1.获取client对象 ---> 即创建OpenAI类对象
 client = OpenAI(
-    api_key=os.environ.get("DASHSCOPE_API_KEY"),
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
 
@@ -12,10 +12,13 @@ response = client.chat.completions.create(
     model="qwen3-max",
     messages=[
         {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "磁通量公式"}
-
-    ]
+        {"role": "user", "content": "你好"}
+    ],
+    stream=True
 )
 
 # 3.处理结果
-print(response.choices[0].message.content)
+for chunk in response:
+    if chunk.choices[0].delta.content:
+        print(chunk.choices[0].delta.content, end="", flush=True)
+
